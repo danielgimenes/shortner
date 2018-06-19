a847361436fc72c0e5e1f8dd13e538366fe14d59
Daniel Gimenes <danielcgimenes@gmail.com>
2018-06-18 23:40:29 -0300

    Main screen with buttons to Create Alias and Get Original Link screens

---
+++ app/src/main/java/com/dgimenes/shortner/GetLinkActivity.kt
@@ -0,0 +1,13 @@
+package com.dgimenes.shortner
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class GetLinkActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_get_link)
+    }
+
+}
---
+++ app/src/main/java/com/dgimenes/shortner/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.dgimenes.shortner
+
+import android.app.Activity
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.reflect.KClass
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupButtons()
+    }
+
+    private fun setupButtons() {
+        create_alias_button.setOnClickListener {
+            openScreen(CreateAliasActivity::class)
+        }
+        get_link_button.setOnClickListener {
+            openScreen(GetLinkActivity::class)
+        }
+    }
+
+    private fun openScreen(activityKlass: KClass<out Activity>) {
+        val intent = Intent(this, activityKlass.java)
+        startActivity(intent)
+    }
+}
